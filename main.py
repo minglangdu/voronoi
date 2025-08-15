6f08e208757543e4f1c3554160d34b7ee07d3cd2
minglangdu <mldu@cydu.net>
2025-08-15 16:48:27 -0700

    begin adding intersections

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import threading
 import os
 from queue import PriorityQueue
 import math
+import bisect
 
-POINT_AMOUNT = 20
+POINT_AMOUNT = 5
 POINT_SIZE = 5
 POINT_DIST = 3
 SCREEN_SIZE = 600
@@ -20,29 +21,86 @@ root.focus_force()
 
 eoi = PriorityQueue()
 points = []
-beachline = set() 
+beachline = []
 directrix = 0;
 for i in range(POINT_AMOUNT):
     point = (random.randint(MARGIN, SCREEN_SIZE - MARGIN), random.randint(MARGIN, SCREEN_SIZE - MARGIN))
-    canv.create_oval(point[0], point[1], point[0] + POINT_SIZE, point[1] + POINT_SIZE, fill="black")
+    canv.create_oval(point[0], point[ 1], point[0] + POINT_SIZE, point[1] + POINT_SIZE, fill="black")
     eoi.put((point[1], point, True)) # (y, (x, y), isFoci)
     points.append(point)
-eoi.put((SCREEN_SIZE, (0, SCREEN_SIZE), True))
+
+class HalfEdge: # ray
+    def __init__(self, x, y, a1, a2):
+        self.x1 = x
+        self.y1 = y
+        # ray is perpendicular to line between arc foci
+        aslope = float(a1.y - a2.y) / float(a1.x - a2.x)
+        self.slope = -1.0 / aslope 
+
+    def draw(self, canv, x2, y2):
+        canv.create_line(self.x1, self.y1, x2, y2)
+
+    def collide(self, arc, dir): # returns intersection point between arc and edge (or -1e9, -1e9) if out of bounds
+        # cy = ((cx - h) ** 2) / (4 * p) + k
+        # cy = sx (s = self.slope)
+        # (cx - h)^2 = 4p(-k + sx)
+        p, h, k = arc.phk(dir)
+        
+
+
+class Edge: # full edge – won't grow
+    def __init__(self, he, ex, ey):
+        self.x1 = he.x1
+        self.y1 = he.y1
+        self.x2 = ex
+        self.y2 = ey
+
+    def draw(self, canv):
+        canv.create_line(self.x1, self.y1, self.x2, self.y2)
+
+    def collide(self, arc, dir): 
+        pass
 
 class Arc:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.ed = [] # full edges – edges that will not grow
+        self.he = [] # half edges – rays extending
+
+    def phk(self, dir):
+        p = (self.y - dir) / 2
+        h = self.x
+        k = (self.y + dir) / 2
+        return [p, h, k]
+    
+    def collide(self, arc, dir):
+        # get collision point between arc and arc or otherwise returns -1e9
+        p1, h1, k1 = self.phk(dir)
+        p2, h2, k2 = arc.phk(dir)
+        # y = ((x - h)^2 / 4p + k)
+        
+        return -1e9
+
+    def getsides(self, dir): # returns x-coordinates where y = 0
+        # p always negative, since dir always > focus
+        p, h, k = self.phk(dir)
+        c = math.sqrt(-4 * p * k) # always positive, as p always negative
+        prev =  [h - c, h + c] # before taking in accound full edges and half edges
+        # half edges
+
+        # full edges
+
+        return prev
 
     def draw(self, canv, dir):
         # dir is directrix
         prev = [-1e9, -1e9]
-        p = (self.y - dir) / 2
-        h = self.x
-        k = (self.y + dir) / 2
+        p, h, k = self.phk(dir)
         if (p == 0):
             return # division by 0
-        for cx in range(0, SCREEN_SIZE, POINT_DIST):
+        s1, s2 = [math.floor(x) for x in self.getsides(dir)]
+        for cx in range(s1, s2, POINT_DIST):
             # 4p(y - k) = (x - h)^2
             # y = ((x - h)^2 / 4p + k)
             # (h, k) is vertex
@@ -60,33 +118,37 @@ def update_aux():
     global points, directrix
     canv.create_rectangle(0, 0, SCREEN_SIZE, SCREEN_SIZE, fill="white")
     for point in points:
-        canv.create_oval(point[0], point[1], point[0] + POINT_SIZE, point[1] + POINT_SIZE, fill="red", outline="red")
+        canv.create_oval(point[0], point[1], point[0] + POINT_SIZE, point[1] + POINT_SIZE, fill="blue", outline="blue")
     for arc in beachline:
         arc.draw(canv, directrix)
 
 def update_sweep(stop):
     if not stop.is_set():
         update_aux()
-        update_sweep_aux()
+        update_sweep_aux(stop)
         threading.Timer(0.001 * WAIT_TIME, lambda:update_sweep(stop)).start()
     else:
         print("stopped")
 
-def update_sweep_aux():
+def update_sweep_aux(stop):
     global eoi
     if (eoi.empty()):
+        stop.set()
         return
     e = eoi.get()
     cur = e[1]
+    global directrix
+    directrix = cur[1]
     if (e[2]):
         # is foci
         canv.create_line(0, cur[1], SCREEN_SIZE, cur[1], fill="red")
         belowArc = None # arc below it
         l = 0
         r = len(beachline)
+        m = -1
         while (r - l > 1):
             m = (l + r) / 2
-            if True: # placeholder
+            if (True):
                 l = m
             else:
                 r = m
@@ -94,12 +156,11 @@ def update_sweep_aux():
         if belowArc:
             # find the intersection point. 
             # resulting line is perpendicular to line between two sites.
-            arc = Arc(cur[0], cur[1]) # placeholder
+            arc = Arc(cur[0], cur[1]) 
         else:
             arc = Arc(cur[0], cur[1])
-            beachline.add(arc)
-        global directrix
-        directrix = cur[1]
+            beachline.append(arc)
+            eoi.put((cur[1] + 100, (0, cur[1] + 100), False))
     else:
         # is edge intersection
         canv.create_line(0, cur[1], SCREEN_SIZE, cur[1], fill="blue")
